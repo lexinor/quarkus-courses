e7fbba1689eebb8add5d8e0f670a580505547118
Lexinor <alternonolimit@gmail.com>
2024-12-23 11:30:02 +0100

    Commit latest changes

--- customer/src/main/java/io/lexinor/formations/quarkus/dbaccess/jpa/Customer.java
+++ customer/src/main/java/io/lexinor/formations/quarkus/dbaccess/jpa/Customer.java
@@ -1,18 +1,25 @@
 package io.lexinor.formations.quarkus.dbaccess.jpa;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.time.Instant;
 
 @Entity
+@Table(name = "t_customers")
 public class Customer {
     @Id
     @GeneratedValue
     private Long id;
+    @Column(name = "first_name", length = 50)
     private String firstName;
+    @Column(name = "last_name", length = 50)
     private String lastName;
+    @Column(name = "email", length = 50)
     private String email;
+    @Column(name = "creation_date")
     private Instant creationDate = Instant.now();
 
     public Customer() {
--- vintage-store/src/main/java/io/lexinor/formations/quarkus/dbaccess/panache/model/Publisher.java
+++ vintage-store/src/main/java/io/lexinor/formations/quarkus/dbaccess/panache/model/Publisher.java
@@ -1,13 +1,18 @@
 package io.lexinor.formations.quarkus.dbaccess.panache.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import java.time.Instant;
 
 
 @Entity
+@Table(name = "t_publishers")
 public class Publisher extends PanacheEntity {
+    @Column(length = 50, nullable = false)
     public String name;
+    @Column(name = "creation_date")
     public Instant createdDate = Instant.now();
 
     public Publisher() {
